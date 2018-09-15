794894813a584d5c304874683f8c8203eedfdbfa
Hawoly Ba <hawolyba@gmail.com>
2018-09-15 21:24:52 +0200

    Adjust responsiveness of ellipse and hero content

--- dist/js/main.js
+++
@@ -1,1 +0,0 @@
-var contactForm=document.querySelector("#contact-form"),newsletter=document.querySelector("#newsletter-form");contactForm.addEventListener("submit",function(e){e.preventDefault()}),newsletter.addEventListener("submit",function(e){e.preventDefault()});
