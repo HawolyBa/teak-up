8041bfa3aa92277e24d139d409d5d9b7b818ad27
Hawoly Ba <hawolyba@gmail.com>
2018-09-15 02:18:32 +0200

    Initialize project

---
+++ gulpfile.js
@@ -0,0 +1,53 @@
+//Import all dependencies
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const imagemin = require('gulp-imagemin');
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const del = require('del');
+const browserSync = require('browser-sync').create();
+
+
+// Create an array of all js files
+
+const jsFiles = [
+  'src/js/main.js'
+]
+// Create all the tasks
+
+gulp.task('sass', function() {
+  gulp.src('src/sass/*.scss') // Indicate the sass folder
+  .pipe(sass().on('error', sass.logError)) // Catch errors 
+  .pipe(autoprefixer("last 2 versions")) // Add the autoprefixer dependency
+  .pipe(gulp.dest('dist/css')) // And finally indicate where to compile the css file
+  .pipe(browserSync.stream())
+})
+
+gulp.task('imagemin', function() {
+  gulp.src('src/img/*') // Minify every images in the folder
+  .pipe(imagemin()) // calls the actual imagemin function 
+  .pipe(gulp.dest('dist/img')) // Indicate the destination folder of the minified images
+})
+
+gulp.task("scripts", function() {
+  gulp.src(jsFiles) // Specify the source folder
+  .pipe(concat('main.js')) // Call the concat function and the name of the file of all concatenated files
+  .pipe(uglify()) //Call the uglify dependency
+  .pipe(gulp.dest('dist/js')) // Indicate the destination folder
+})
+
+gulp.task("serve", ['sass'], function() {
+  browserSync.init({
+    server: {
+      baseDir: "./dist"
+    }
+  })
+
+  gulp.watch('src/js/*.js', ['scripts']) // Call the watch function for every changes in the js folder
+  gulp.watch('src/img/*', ['imagemin'])
+  gulp.watch('src/sass/**/*', ['sass'])
+  gulp.watch('dist/*.html').on('change', browserSync.reload);
+})
+
+gulp.task('default', ['serve']);
