7667d128c3391fc82946f1056c464d037657dbe7
Hawoly Ba <hawolyba@gmail.com>
2018-09-15 21:04:16 +0200

    Contact section and form submit

---
+++ dist/js/main.js
@@ -0,0 +1,1 @@
+var contactForm=document.querySelector("#contact-form"),newsletter=document.querySelector("#newsletter-form");contactForm.addEventListener("submit",function(e){e.preventDefault()}),newsletter.addEventListener("submit",function(e){e.preventDefault()});
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+var contactForm = document.querySelector('#contact-form')
+var newsletter = document.querySelector('#newsletter-form')
+contactForm.addEventListener('submit', function(e) {
+  e.preventDefault()
+})
+
+newsletter.addEventListener('submit', function(e) {
+  e.preventDefault()
+})
